20efd43ae5b6260cd0aed0caed68eaa46271781e
romanlaptev <roman-laptev@yandex.ru>
2020-03-19 12:05:57 +0700

    up3

--- php/test.php
+++ php/test.php
@@ -1,22 +1,16 @@
 <?php
-//echo "Content-type: text/html\n";
-//echo "\n";
+echo "Content-type: text/html\n";
+echo "\n";
 
 //echo "<pre>";
 //print_r(PDO::getAvailableDrivers());
 //echo "</pre>";
 
-$loadedExt = get_loaded_extensions();
-echo "loaded extensions: <pre>";
-print_r( $loadedExt );
-echo "</pre>";
-
 phpinfo ();
 phpinfo(INFO_MODULES);
 
-
-//for ($n1=0;$n1<10;$n1++){
-//	echo $n1." Hello PHP world!<br>\n";
-//}
+for ($n1=0;$n1<10;$n1++){
+	echo $n1." Hello PHP world!<br>\n";
+}
 ?>
 
