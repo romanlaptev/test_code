7a92cdeda1a92c7924d929c15b3c08d693d44bd8
romanlaptev <roman-laptev@yandex.ru>
2020-03-23 16:14:17 +0700

    update test code, php

--- php/test_array.php
+++ php/test_array.php
@@ -15,4 +15,41 @@ $link = &$first;
 $link = 77;
 
 echo $first."\n";//77
+
+//======================
+$some_var = "Hello PHP!!!";
+$arr1 = (array)$some_var;
+print_r( $arr1 );
+echo "\n";
+
+//================
+$arr1 = [ 1 => "one", 2 => "two", 3 => "three" ];
+$arr2 = [ 4 => "four", 5 => "five", 1 => "one" ];
+
+$arr3 = $arr1 + $arr2;
+print_r( $arr3 );
+echo "\n";
+
+$arr4 = array_merge ($arr1, $arr2);
+print_r( $arr4 );
+echo "\n";
+
+//===============
+$arr1 = [ 1 => "one", 2 => "two", 3 => "three" ];
+$arr2 = [ 1 => "one", 2 => "two", 3 => "three" ];
+if( $arr1 === $arr2 ){
+echo "arr1 == ar2...";
+echo "\n";
+} else {
+echo "arr1 != ar2...";
+echo "\n";
+}
+
+ //=======================
+echo "Arr1 is array: ". is_array($arr1);
+echo "\n";
+
+echo "Value 'two' in arr1: ". in_array( "two", $arr1);
+echo "\n";
+
 ?>
