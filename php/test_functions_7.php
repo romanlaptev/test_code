ff61d4b69a93ee3a2405983c572ee1dd53c58b53
romanlaptev <roman-laptev@yandex.ru>
2020-03-24 12:01:57 +0700

    update test code, php

---
+++ php/test_functions_7.php
@@ -0,0 +1,19 @@
+<?php
+echo PHP_VERSION;//5.6.40-0+deb8u7
+echo "<br/>\n";
+//echo phpversion();
+//echo "<br/>\n";
+
+
+//========================
+$c = func3();
+echo "c = ".$c;
+echo "<br/>\n";
+
+function func3() : int //PHP 7
+{
+	$_a = 8;
+	return ++$_a;
+}//end
+
+?>
