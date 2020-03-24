c3bdac91c910260a9f8bef63e5d3b700947811a8
romanlaptev <roman-laptev@yandex.ru>
2020-03-24 10:04:50 +0700

    update test code, php

---
+++ php/test_functions.php
@@ -0,0 +1,33 @@
+<?php
+//send variable $a to the function as link
+$a = 1;
+
+$b = func1( $a );
+
+echo "a = ".$a;//2, function func1( &$a )!!!!!
+echo "<br/>\n";
+
+echo "b = ".$b;//2
+echo "<br/>\n";
+
+function func1( &$a ){//!!!!!
+	return ++$a;
+}
+
+//========================
+$arr1 = [1, 2, 3];
+
+func2( $arr1 );
+
+echo "<pre>\n";
+print_r( $arr1 );
+echo "</pre>\n";
+
+function func2( array $arr1 ){
+	$arr1[0] = 4;
+	$arr1[1] = 5;
+	$arr1[2] = 6;
+}//end
+
+
+?>
