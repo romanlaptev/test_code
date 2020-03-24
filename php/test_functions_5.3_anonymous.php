96987bb13d1dd28c8ff8faf81d0f38a1b92ea30b
romanlaptev <roman-laptev@yandex.ru>
2020-03-24 13:00:10 +0700

    update test code, php

--- php/test_functions_5.3_anonymous.php
+++ php/test_functions_5.3_anonymous.php
@@ -4,34 +4,6 @@ echo "<br/>\n";
 //echo phpversion();
 //echo "<br/>\n";
 
-$res = version_compare( PHP_VERSION, '5.3.0' );
-echo "version_compare: ". $res;
-echo "<br/>\n";
-
-switch( $res ){
-
-	case -1:
-//echo PHP_VERSION;
-echo phpversion();
-echo "Your PHP version < 5.3.0" ;
-echo "<br/>\n";
-exit;
-	break;
-/*
-	case 0:
-echo phpversion();
-echo "Your PHP version === 5.6.0" ;
-echo "<br/>\n";
-	break;
-
-	case 1:
-echo phpversion();
-echo "Your PHP version > 5.6.0" ;
-echo "<br/>\n";
-	break;
-*/
-}//end switch
-
 //====================== anonymous function
 // PHP => 5.3.0
 //https://www.php.net/manual/ru/functions.anonymous.php
