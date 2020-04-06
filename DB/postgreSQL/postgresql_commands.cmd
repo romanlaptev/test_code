b1c59ede64f61ecd5a353fd129ac92797cea19dd
romanlaptev <roman-laptev@yandex.ru>
2020-04-06 11:25:05 +0700

    update test code, postgresql

--- DB/postgreSQL/postgresql_commands.cmd
+++ DB/postgreSQL/postgresql_commands.cmd
@@ -1,1 +1,4 @@
-pg_dump -U postgres -W -F p db1 > dump_db1.sql
+rem pg_dump -U postgres -W -F p db1 > dump_db1.sql
+psql -U postgres  db1 < notes_postgresql.sql
+pg_restore -d db1 ./notes_postgresql.sql -U postgres
+
