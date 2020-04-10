fe561a87c5a08a26e99ac28005a243142d76b0de
eee-pc user <roman-laptev@yandex.ru>
2020-04-10 21:07:29 +0600

    update php code

---
+++ php/crop_filters.php
@@ -0,0 +1,52 @@
+﻿<?php 
+echo "<pre>";
+// print_r ($_SERVER);
+// print_r ($_REQUEST);
+// //print_r($_FILES);
+print_r( gd_info() );
+echo "</pre>";
+
+//https://www.php.net/manual/ru/refs.utilspec.image.php
+//https://www.php.net/manual/ru/intro.image.php
+//https://www.php.net/manual/ru/function.imagecreate.php
+//http://www.php.su/imagecreate
+
+//https://www.php.net/manual/ru/function.gd-info.php
+/*
+header ("Content-type: image/png");
+
+$im = @imagecreate (200, 200)or die ("Cannot Initialize new GD image stream");
+$im2 = @imagecreate (200, 200)or die ("Cannot Initialize new GD image stream");
+
+$background_color = imagecolorallocate ($im, 255, 255, 255);
+$text_color = imagecolorallocate ($im, 233, 14, 91);
+$color2 = imagecolorallocate ($im, 233, 14, 91);
+
+imagestring ($im, 1, 5, 5,"Circle", $text_color);
+
+$x0 = 100;
+$y0 = 100;
+$px = 60;
+$py = 60;
+for ($t = 0; $t < 360; $t++)
+{
+	$x = cos($t) * $px;
+	$y = sin($t) * $py;
+	$x = $x + $x0;
+	$y = $y + $y0;
+	imagesetpixel ($im, $x, $y, $color2);
+}//next
+
+imagepng ($im);
+*/
+
+/*
+header("Content-Type: image/png");
+$im = @imagecreate(110, 20) or die("Cannot Initialize new GD image stream");
+$background_color = imagecolorallocate($im, 255, 255, 255);
+$text_color = imagecolorallocate($im, 233, 14, 91);
+imagestring($im, 1, 5, 5,  "A Simple Text String", $text_color);
+imagepng($im);
+//imagedestroy($im);
+*/
+?>
