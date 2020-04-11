07880b6bc60e7a2fcc597b5a908a83596877ab57
romanlaptev <roman-laptev@yandex.ru>
2020-04-11 12:47:06 +0700

    update php code

--- php/utils.php
+++ php/utils.php
@@ -8,6 +8,44 @@
 //ini_set('display_errors', 1);
 
 /*
+//https://www.php.net/manual/en/function.version-compare.php
+//if (version_compare(PHP_VERSION, '5.0.0', '>=')) {
+    //echo 'I am at least PHP version 5.0.0, my version: ' . PHP_VERSION . "\n";
+//}
+//By default, version_compare() returns -1 if the first version is lower than the second, 0 if they are equal, and 1 if the second is lower. 
+
+$required_ver = '5.1.0';
+$res = version_compare( PHP_VERSION,  $required_ver );
+echo "version_compare: ". $res;
+echo "<br/>\n";
+
+switch( $res ){
+
+	case -1:
+//echo PHP_VERSION;
+echo phpversion();
+echo "Your PHP version < ".$required_ver ;
+echo "<br/>\n";
+	break;
+
+	case 0:
+echo phpversion();
+echo "Your PHP version === ".$required_ver ;
+echo "<br/>\n";
+	break;
+
+	case 1:
+echo phpversion();
+echo "Your PHP version > ".$required_ver ;
+echo "<br/>\n";
+	break;
+
+}//end switch
+
+
+*/
+
+/*
 $sapi_type = php_sapi_name();
 
 $_vars["runType"] = "";
@@ -22,68 +60,96 @@ if ( $sapi_type == 'cgi') {
 	$_vars["runType"] = "console";
 }
 $msg = "Run method (php_sapi_name): ". $sapi_type;
-echo logAlert($msg, "info");
+echo _log( $msg, "info" );
 */
 
 
 //============================
-function logAlert( $msg, $level){
-//global $sapi_type;
-global $_vars;
+function _log( $msg, $level){
+
+	// check API type
+	$sapi_type = php_sapi_name();
+//echo "php_sapi_name: ". $sapi_type;
+//echo "<br/>\n";
+//echo "type: ". gettype( $msg);
+//echo "<br/>\n";
+
+//-------------
+	$runType = "";
+	if ( $sapi_type == 'apache2handler' || 
+			$sapi_type == 'cli-server'
+		) {
+		$runType = "web";
+	}
+
+	if ( $sapi_type == "cli" ) { $runType = "console"; }
+	if ( $sapi_type == "cgi" ) { $runType = "console"; }
+
+//-------------
+	if( gettype( $msg) === "array"){
+			if ( $runType == "web" ) {
+				$out = "<pre>".print_r($msg,1)."</pre>";
+				return $out;
+			} else {
+				$out = print_r($msg,1)."\n";
+				return $out;
+			}
+	}
+
+	if( gettype( $msg) !== "string"){
+		return false;
+	}
+
+//-------------
 	switch ($level) {
 		case "info":
-			if ( $_vars["runType"] == "web" ) {
+			if ( $runType == "web" ) {
 				return "<div class='alert alert-info'>".$msg."</div>";
 			}
-			if ( $_vars["runType"] == "console" ) {
+			if ( $runType == "console" ) {
 				return $msg."\n";
 			}
 		break;
 		
 		case "warning":
-			if ( $_vars["runType"] == "web" ) {
+			if ( $runType == "web" ) {
 				return "<div class='alert alert-warning'>".$msg. "</div>";
 			}
-			if ( $_vars["runType"] == "console" ) {
+			if ( $runType == "console" ) {
 				return $msg."\n";
 			}
 		break;
 		
 		case "danger":
 		case "error":
-			if ( $_vars["runType"] == "web" ) {
+			if ( $runType == "web" ) {
 				return "<div class='alert alert-danger'>".$msg. "</div>";
 			}
-			if ( $_vars["runType"] == "console" ) {
+			if ( $runType == "console" ) {
 				return $msg."\n";
 			}
 		break;
 		
 		case "success":
-			if ( $_vars["runType"] == "web" ) {
+			if ( $runType == "web" ) {
 				return "<div class='alert alert-success'>".$msg. "</div>";
 			}
-			if ( $_vars["runType"] == "console" ) {
+			if ( $runType == "console" ) {
 				return $msg."\n";
 			}
 		break;
 		
 		default:
-			if ( $_vars["runType"] == "web" ) {
+			if ( $runType == "web" ) {
 				return $msg. "<br/>";
 			}
-			if ( $_vars["runType"] == "console" ) {
+			if ( $runType == "console" ) {
 				return $msg."\n";
 			}
 		break;
 	}//end switch
 
-}//end logAlert()
+}//end _log()
 
-
-function _log($arr){
-	$out = "<pre>".print_r($arr,1)."</pre>\n";
-	return $out;
-}
 
 ?>
