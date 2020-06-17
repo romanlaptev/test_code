48202e91fd27c0a72f22fa3ad57b7f1f76318411
romanlaptev <roman-laptev@yandex.ru>
2020-06-17 10:47:40 +0700

    update test code

--- php/test_json.php
+++ php/test_json.php
@@ -84,5 +84,103 @@ echo "</pre>\n";
 
 */
 
+/*
+	public function rpc_list(){
+		
+		if ( !function_exists("json_encode") ){//PHP 5 >= 5.2.0
+			$eventType = "error";
+			$message = "error, not support function <b>json_encode()</b>. wrong PHP version - ".phpversion().", need PHP >= 5.2.0";
+			$jsonStr = '{"eventType": "'.$eventType.'", "message": "'.$message.'"}';
+			echo $jsonStr;
+			exit ();
+		}
+		
+		$contentArr = $content->getListWithType();
+//echo _logWrap($contentArr);
+
+		$json_string = json_encode( $contentArr );
+		if ( !function_exists("json_last_error") ){ //PHP 5 >= 5.3.0
+			$eventType = "error";
+			//http://php.net/manual/ru/function.json-encode.php
+			$message = "<p>error, not support function <b>json_last_error()</b>. wrong PHP version - ".phpversion().", need PHP >= 5.3.0</p>";
+			$jsonStr = '{"eventType": "'.$eventType.'", "message": "'.$message.'"}';
+			echo $jsonStr;
+			exit ();
+		}
+
+//https://www.php.net/manual/en/function.json-last-error.php
+		switch ( json_last_error() ) {
+			case JSON_ERROR_NONE:
+				$eventType = "success";
+			break;
+			
+			case JSON_ERROR_DEPTH:
+$eventType = "error";
+$message = "The maximum stack depth has been exceeded";
+			break;
+
+			case JSON_ERROR_STATE_MISMATCH:
+$eventType = "error";
+$message = "Invalid or malformed JSON";
+			break;
+
+			case JSON_ERROR_CTRL_CHAR:
+$eventType = "error";
+$message = "Control character error, possibly incorrectly encoded";
+			break;
+
+			case JSON_ERROR_SYNTAX:
+$eventType = "error";
+$message = "Syntax error";
+			break;
+			
+			case JSON_ERROR_UTF8:
+$eventType = "error";
+$message = "Malformed UTF-8 characters, possibly incorrectly encoded";
+			break;
+			
+			case JSON_ERROR_RECURSION:
+$eventType = "error";
+$message = "One or more recursive references in the value to be encoded";
+//PHP 5.5.0
+			break;
+
+			case JSON_ERROR_INF_OR_NAN:
+$eventType = "error";
+$message = "One or more NAN or INF values in the value to be encoded";
+//PHP 5.5.0
+			break;
+
+			case JSON_ERROR_UNSUPPORTED_TYPE:
+$eventType = "error";
+$message = "A value of a type that cannot be encoded was given";
+//PHP 5.5.0
+			break;
+
+			case JSON_ERROR_INVALID_PROPERTY_NAME:
+$eventType = "error";
+$message = "A property name that cannot be encoded was given";
+//PHP 7.0.0
+			break;
+
+			case JSON_ERROR_UTF16:
+$eventType = "error";
+$message = "Malformed UTF-16 characters, possibly incorrectly encoded";
+//PHP 7.0.0
+			break;
+			
+			default:
+$eventType = "error";
+$message = "json_last_error(), Unknown error";
+			break;
+		}//end switch
+		
+		$jsonStr = '{"eventType": "'.$eventType.'", "message": "'.$message.'"}';
+		echo $jsonStr;
+		
+		exit();
+	}//end rpc_list()
+
+*/
 ?>
 
