a585bef145c23b5c6d1ac69e5f04dc260a2d3bef
romanlaptev <romanlaptev@8points.ru>
2020-07-24 17:13:10 +0700

    update test php

---
+++ php/test_simple_html_dom/test.php
@@ -0,0 +1,23 @@
+<?php
+//error_reporting(E_ALL|E_STRICT);
+//ini_set('display_errors', 1);
+
+//php -S localhost:8000
+	require_once( __DIR__.'/../utils.php' );
+
+//https://xdan.ru/uchimsya-parsit-saity-s-bibliotekoi-php-simple-html-dom-parser.html
+//Учимся парсить сайты с библиотекой PHP Simple HTML DOM Parser
+
+//https://simplehtmldom.sourceforge.io/manual.htm#section_quickstart
+//PHP Simple HTML DOM Parser Manual
+
+	//require_once( __DIR__.'/simple_html_dom.php' );
+	
+	//$dom = str_get_html('<html><body><div class="con-tent">Hello!</div></body></html>');
+//$test = $dom->find(".con-tent");
+
+	//$out = print_r($test, true);
+	//echo _logWrap($out);
+	
+echo _logWrap("tesT", "info");
+?>
